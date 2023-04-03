34030a11956272ad03c63a33db23d2d90dfd0acb
Jorge Daniel Fernando Falla Gastulo <jfallaga@emeal.nttdata.com>
2023-04-03 10:03:56 -0500

    configuración personalizada del tema de chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React, {StrictMode} from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './common'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={ theme }>
+      <QueryClientProvider client={ queryClient }>
+        <App />
+      </QueryClientProvider>
+    </ChakraProvider>
   </StrictMode>,
 )
