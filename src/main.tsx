bd205f20396d0ba7accf07dd0c1b46f40e895638
Jorge Daniel Fernando Falla Gastulo <jfallaga@emeal.nttdata.com>
2023-04-03 11:05:24 -0500

    ReactQueryDevtools agregado

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { queryClient } from './common'
 import { ChakraProvider } from '@chakra-ui/react'
+
 import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ChakraProvider theme={ theme }>
       <QueryClientProvider client={ queryClient }>
         <App />
+        <ReactQueryDevtools initialIsOpen/>
       </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>,
