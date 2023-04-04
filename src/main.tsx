2e503bbfa155eace57e77baf443ba9e4b171034d
Jorge Daniel Fernando Falla Gastulo <fenan.03.95@gmail.com>
2023-04-04 12:55:37 -0500

    initialIsOpen en false para que no se habra al momento de iniciar la aplicacion

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Provider store = { store }>
           <AppRouter />
         </Provider>
-        <ReactQueryDevtools initialIsOpen/>
+        <ReactQueryDevtools initialIsOpen={false}/>
       </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>,
