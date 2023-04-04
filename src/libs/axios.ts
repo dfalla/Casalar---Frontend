89d18745d583e528acc248f44fc9dc7e659d37ea
Jorge Daniel Fernando Falla Gastulo <fenan.03.95@gmail.com>
2023-04-04 11:48:16 -0500

    configuracion del store de zustand

---
+++ src/libs/axios.ts
@@ -0,0 +1,29 @@
+import Axios from "axios";
+import { useAuthStore } from "../store/auth";
+
+// const baseURL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.DOMAIN
+//     : "http://localhost:3000";
+
+const baseURL = "http://localhost:8000/api"
+ 
+
+const api = Axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+    const token = useAuthStore.getState().token;
+    if(config && config.headers){
+        config.headers['x-token'] = {
+          Authorization: `Bearer ${token}`,
+        };
+
+    }
+  
+  return config;
+});
+
+export default api;
---
+++ src/store/auth.ts
@@ -0,0 +1,73 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+import { SafeAny } from "../common";
+import { profileRequest, registerRequest } from "../api";
+
+export interface Profile {
+    _id?: string;
+    email?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    __v?: number;
+}
+
+
+export interface createUser {
+    email: string;
+    password: string;
+    fullName: string;
+}
+
+type State = {
+    token: string | null;
+    profile: Profile;
+    isAuth: boolean;
+    errors: SafeAny;
+};
+
+type Actions = {
+    setToken: (token: string) => void;
+    register: (user: createUser) => void;
+    logout: () => void;
+    cleanErrors: () => void;
+};
+
+export const useAuthStore = create(
+    persist<State & Actions>(
+      (set) => ({
+        token: null,
+        profile: {},
+        isAuth: false,
+        errors: null,
+        setToken: (token: string) =>
+          set((state) => ({
+            token,
+            isAuth: !!token,
+          })),
+        register: async (user: createUser) => {
+          try {
+            const resRegister = await registerRequest(user);
+            set(() => ({
+              token: resRegister.data.token,
+              isAuth: true,
+            }));
+          } catch (error) {
+            set(() => ({ errors: (error as SafeAny).response.data }));
+          }
+        },
+        getProfile: async () => {
+          const resProfile = await profileRequest();
+          set(() => ({
+            profile: resProfile.data,
+          }));
+        },
+        logout: () => set(() => ({ token: null, profile: {}, isAuth: false })),
+        cleanErrors: () => set(() => ({ errors: null })),
+      }),
+      {
+        name: "auth",
+      }
+    )
+  );
+
+
